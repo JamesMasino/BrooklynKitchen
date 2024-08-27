f9c6373ec392bbdd6018dae46959d4e6ff9769ed
James <james@masino.art>
2024-08-27 00:45:29 -0400

    Performance upgrade branch initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,31 +10,39 @@ import {
   Stove,
   UpperCabinets
 } from "@components";
+import { AdaptiveDpr, Preload } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useWindowSize } from "@uidotdev/usehooks";
+import { Suspense } from "react";
 
 export default function App() {
   const { width, height } = useWindowSize();
 
   return (
-    <Canvas
-      shadows={true}
-      camera={{
-        fov: 45,
-        position: [10, 1, 11],
-        zoom: 3.3
-      }}>
-      <Environment>
-        <KitchenRoom />
-        <CoffeePot />
-        <CoffeeMug />
-        <CoffeeMaker />
-        <UpperCabinets />
-        <LowerCabinetSink />
-        <Fridge />
-        <Stove />
-        <Sink />
-      </Environment>
-    </Canvas>
+    <Suspense fallback={<div className="suspense"></div>}>
+      <Canvas
+        performance={{ min: 0.5 }}
+        frameloop="demand"
+        shadows={true}
+        camera={{
+          fov: 45,
+          position: [10, 1, 11],
+          zoom: 3.3
+        }}>
+        <Environment>
+          <KitchenRoom />
+          <CoffeePot />
+          <CoffeeMug />
+          <CoffeeMaker />
+          <UpperCabinets />
+          <LowerCabinetSink />
+          <Fridge />
+          <Stove />
+          <Sink />
+          <Preload all />
+        </Environment>
+        <AdaptiveDpr pixelated />
+      </Canvas>
+    </Suspense>
   );
 }

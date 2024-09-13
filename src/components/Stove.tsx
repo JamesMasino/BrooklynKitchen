4d0c84a6d4565b48e29a9efb9a1764f327d64ded
James <james@masino.art>
2024-09-13 00:54:30 -0400

    LOD support added for stove, stove geo and lookdev fix

--- src/components/Stove.tsx
+++ src/components/Stove.tsx
@@ -1,344 +1,213 @@
-import { useGLTF } from "@react-three/drei";
+import { Detailed, useGLTF } from "@react-three/drei";
 import { useConfig } from "@store";
 import extend from "just-extend";
 import { useControls } from "leva";
+import { ShadowedMesh } from "./Environment";
 
 const config = {
   "model": "stove.glb",
   "512": "/stove/512",
-  "quality": "512"
+  "1k": "/stove/1k",
+  "2k": "/stove/2k",
+  "LOD512": "512",
+  "LOD1k": "1k",
+  "LOD2k": "2k"
+};
+const configs = {
+  LOD512: config.LOD512,
+  LOD1k: config.LOD1k,
+  LOD2k: config.LOD2k
 };
 
 export function Stove(props) {
   const stove = useConfig(e => e.Stove);
-  const { nodes, materials } = useGLTF(
-    `${config[config.quality]}/${config.model}`
+  const { nodes: nodesLOD512, materials: materials512 } = useGLTF(
+    `${config[configs.LOD512]}/${config.model}`
+  );
+  const { nodes: nodesLOD1k, materials: materials1k } = useGLTF(
+    `${config[configs.LOD1k]}/${config.model}`
+  );
+  const { nodes: nodesLOD2k, materials: materials2k } = useGLTF(
+    `${config[configs.LOD2k]}/${config.model}`
   );
   const stoveProps = useControls("Stove", extend(true, stove, {}), {
     collapsed: true
   });
   return (
-    <group {...props} position={[0, 0, 0]} dispose={null}>
-      <group scale={0.01}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.stoveBase_1["geometry"]}
-          material={materials.stoveBase_mtl_1001}
+    <group
+      {...props}
+      position={[0, 0, 0]}
+      visible={stoveProps["stove"]}
+      scale={0.01}
+      dispose={null}>
+      <Detailed distances={[0, 2, 3]}>
+        <ShadowedMesh
+          geometry={nodesLOD2k.stoveBase_1["geometry"]}
+          material={materials2k.stoveBase_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.stoveTop_1["geometry"]}
-          material={materials.stoveTop_mtl_1001}
+        <ShadowedMesh
+          geometry={nodesLOD1k.stoveBase_1["geometry"]}
+          material={materials1k.stoveBase_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.exhaust_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
+        <ShadowedMesh
+          geometry={nodesLOD512.stoveBase_1["geometry"]}
+          material={materials512.stoveBase_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.burner01_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
+      </Detailed>
+      <Detailed distances={[0, 2, 3]}>
+        <ShadowedMesh
+          geometry={nodesLOD2k.handles_1["geometry"]}
+          material={materials2k.sectionMetals_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.burner02_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
+        <ShadowedMesh
+          geometry={nodesLOD1k.handles_1["geometry"]}
+          material={materials1k.sectionMetals_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.burner03_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
+        <ShadowedMesh
+          geometry={nodesLOD512.handles_1["geometry"]}
+          material={materials512.sectionMetals_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.burner04_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
+      </Detailed>
+      <Detailed distances={[0, 2, 3]}>
+        <ShadowedMesh
+          geometry={nodesLOD2k.stoveDrawer_1["geometry"]}
+          material={materials2k.sectionMetals_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.tr_prong1_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
+        <ShadowedMesh
+          geometry={nodesLOD1k.stoveDrawer_1["geometry"]}
+          material={materials1k.sectionMetals_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.tr_prong2_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
+        <ShadowedMesh
+          geometry={nodesLOD512.stoveDrawer_1["geometry"]}
+          material={materials512.sectionMetals_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.tr_prong3_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
+      </Detailed>
+      <Detailed distances={[0, 2, 3]}>
+        <ShadowedMesh
+          geometry={nodesLOD2k.feet_1["geometry"]}
+          material={materials2k.grateMetals_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.tr_prong4_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
+        <ShadowedMesh
+          geometry={nodesLOD1k.feet_1["geometry"]}
+          material={materials1k.grateMetals_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.tr_base_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
+        <ShadowedMesh
+          geometry={nodesLOD512.feet_1["geometry"]}
+          material={materials512.grateMetals_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.br_prong1_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
+      </Detailed>
+      <Detailed distances={[0, 2, 3]}>
+        <ShadowedMesh
+          geometry={nodesLOD2k.stoveTop_1["geometry"]}
+          material={materials2k.stoveTop_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.br_prong2_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
+        <ShadowedMesh
+          geometry={nodesLOD1k.stoveTop_1["geometry"]}
+          material={materials1k.stoveTop_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.br_prong3_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
+        <ShadowedMesh
+          geometry={nodesLOD512.stoveTop_1["geometry"]}
+          material={materials512.stoveTop_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.br_prong4_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
+      </Detailed>
+      <Detailed distances={[0, 2, 3]}>
+        <ShadowedMesh
+          geometry={nodesLOD2k.burnerGrates_1["geometry"]}
+          material={materials2k.grateMetals_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.br_base_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
+        <ShadowedMesh
+          geometry={nodesLOD1k.burnerGrates_1["geometry"]}
+          material={materials1k.grateMetals_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.tl_prong1_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
+        <ShadowedMesh
+          geometry={nodesLOD512.burnerGrates_1["geometry"]}
+          material={materials512.grateMetals_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.tl_prong2_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
+      </Detailed>
+      <Detailed distances={[0, 2, 3]}>
+        <ShadowedMesh
+          geometry={nodesLOD2k.stovePanel_1["geometry"]}
+          material={materials2k.sectionMetals_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.tl_prong3_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
+        <ShadowedMesh
+          geometry={nodesLOD1k.stovePanel_1["geometry"]}
+          material={materials1k.sectionMetals_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.tl_prong4_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
+        <ShadowedMesh
+          geometry={nodesLOD512.stovePanel_1["geometry"]}
+          material={materials512.sectionMetals_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.tl_base_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
+      </Detailed>
+      <Detailed distances={[0, 2, 3]}>
+        <ShadowedMesh
+          geometry={nodesLOD2k.knobs_1["geometry"]}
+          material={materials2k.knobs_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.bl_prong1_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
+        <ShadowedMesh
+          geometry={nodesLOD1k.knobs_1["geometry"]}
+          material={materials1k.knobs_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.bl_prong2_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
+        <ShadowedMesh
+          geometry={nodesLOD512.knobs_1["geometry"]}
+          material={materials512.knobs_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.bl_prong3_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
+      </Detailed>
+      <Detailed distances={[0, 2, 3]}>
+        <ShadowedMesh
+          geometry={nodesLOD2k.button_1["geometry"]}
+          material={materials2k.knobs_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.bl_prong4_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
+        <ShadowedMesh
+          geometry={nodesLOD1k.button_1["geometry"]}
+          material={materials1k.knobs_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.bl_base_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
+        <ShadowedMesh
+          geometry={nodesLOD512.button_1["geometry"]}
+          material={materials512.knobs_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.stovePanel_1["geometry"]}
-          material={materials.sectionMetals_mtl_1001}
+      </Detailed>
+      <Detailed distances={[0, 2, 3]}>
+        <ShadowedMesh
+          geometry={nodesLOD2k.doorGlass_1["geometry"]}
+          material={materials2k.doorGlass_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.c_knob_1["geometry"]}
-          material={materials.knobs_mtl_1001}
+        <ShadowedMesh
+          geometry={nodesLOD1k.doorGlass_1["geometry"]}
+          material={materials1k.doorGlass_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.c_knobAttachment_1["geometry"]}
-          material={materials.knobs_mtl_1001}
+        <ShadowedMesh
+          geometry={nodesLOD512.doorGlass_1["geometry"]}
+          material={materials512.doorGlass_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.mr_knob_1["geometry"]}
-          material={materials.knobs_mtl_1001}
+      </Detailed>
+      <Detailed distances={[0, 2, 3]}>
+        <ShadowedMesh
+          geometry={nodesLOD2k.stoveDoor_1["geometry"]}
+          material={materials2k.stoveDoor_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.mr_knobAttachment_1["geometry"]}
-          material={materials.knobs_mtl_1001}
+        <ShadowedMesh
+          geometry={nodesLOD1k.stoveDoor_1["geometry"]}
+          material={materials1k.stoveDoor_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.r_knob_1["geometry"]}
-          material={materials.knobs_mtl_1001}
+        <ShadowedMesh
+          geometry={nodesLOD512.stoveDoor_1["geometry"]}
+          material={materials512.stoveDoor_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.r_knobAttachment_1["geometry"]}
-          material={materials.knobs_mtl_1001}
+      </Detailed>
+      <Detailed distances={[0, 2, 3]}>
+        <ShadowedMesh
+          geometry={nodesLOD2k.innerDoor_1["geometry"]}
+          material={materials2k.doorGlass_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.l_knob_1["geometry"]}
-          material={materials.knobs_mtl_1001}
+        <ShadowedMesh
+          geometry={nodesLOD1k.innerDoor_1["geometry"]}
+          material={materials1k.doorGlass_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.l_knobAttachment_1["geometry"]}
-          material={materials.knobs_mtl_1001}
+        <ShadowedMesh
+          geometry={nodesLOD512.innerDoor_1["geometry"]}
+          material={materials512.doorGlass_mtl_1001}
         />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.ml_knob_1["geometry"]}
-          material={materials.knobs_mtl_1001}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.ml_knobAttachment_1["geometry"]}
-          material={materials.knobs_mtl_1001}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.button_1["geometry"]}
-          material={materials.knobs_mtl_1001}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.buttonCasing_1["geometry"]}
-          material={materials.knobs_mtl_1001}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.stoveDoor_1["geometry"]}
-          material={materials.stoveDoor_mtl_1001}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.innerDoor_1["geometry"]}
-          material={materials.doorGlass_mtl_1001}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.outterGlass_1["geometry"]}
-          material={materials.doorGlass_mtl_1001}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.innerGlass_1["geometry"]}
-          material={materials.doorGlass_mtl_1001}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.attachment1_1["geometry"]}
-          material={materials.sectionMetals_mtl_1001}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.attachment2_1["geometry"]}
-          material={materials.sectionMetals_mtl_1001}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.handle_1["geometry"]}
-          material={materials.sectionMetals_mtl_1001}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.stoveDrawer_1["geometry"]}
-          material={materials.sectionMetals_mtl_1001}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.lowerHandle_1["geometry"]}
-          material={materials.sectionMetals_mtl_1001}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.stoveFoot01_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.stoveFoot02_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.stoveFoot03_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.stoveFoot04_1["geometry"]}
-          material={materials.grateMetals_mtl_1001}
-        />
-      </group>
+      </Detailed>
     </group>
   );
 }
